fe5890e603714fc12b2a3ae8e3bdb166066cd089
jbreemes <54798893+jbreemes@users.noreply.github.com>
2021-05-07 13:03:08 +0200

    Start on health
    added health ui
    - need to add health depletion

---
+++ Jump star/Assets/Script/Player/Health.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public GameObject[] hearts;
+    public Sprite FullHP;
+    public Sprite EmptyHP;
+
+
+   
+}
